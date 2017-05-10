3f876c715fa9413221303fc1a8ed1c80ecf91dc3
liqiang <837769474@qq.com>
2017-05-10 15:58:19 +0800

    导入初始项目

---
+++ www/js/app.js
@@ -0,0 +1,137 @@
+// Ionic Starter App
+
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
+
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    })
+    .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+    //解决tabs在Android下局域顶部的方法
+        $ionicConfigProvider.platform.ios.tabs.style('standard');
+        $ionicConfigProvider.platform.ios.tabs.position('bottom');
+        $ionicConfigProvider.platform.android.tabs.style('standard');
+        $ionicConfigProvider.platform.android.tabs.position('bottom');
+        $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+        $ionicConfigProvider.platform.android.navBar.alignTitle('center');
+        $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+        $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
+
+        $ionicConfigProvider.platform.ios.views.transition('ios');
+        $ionicConfigProvider.platform.android.views.transition('android');
+
+        $stateProvider
+            //登录tabs  ivew
+            .state('signin', {
+                url: '/sign-in',
+                templateUrl: 'templates/sign-in.html',
+                cache: 'false',
+                controller: 'SignInCtrl'
+            })
+            //tabs导航路由
+            .state('tab', {
+                url: '/tab',
+                abstract: true,
+                templateUrl: 'templates/tabs.html'
+            })
+
+            //首页
+            .state('tab.home', {
+                url: '/home',
+                views: {
+                    'tab-home': {
+                        templateUrl: 'templates/home.html',
+                        controller: 'HomeCtrl'
+                    }
+                }
+            })
+
+            //  ***首页  大乐透下单详情
+            .state('BigLotto', {
+                url: '/BigLotto',
+                cache: 'false',
+                templateUrl: 'templates/BigLotto.html',
+                controller: 'BigLottoCtrl'
+            })
+
+
+            //推荐
+            .state('tab.recommend', {
+                url: '/recommend',
+                views: {
+                    'tab-recommend': {
+                        templateUrl: 'templates/recommend.html',
+                        controller: 'RecommendCtrl'
+                    }
+                }
+            })
+
+            //资讯
+            .state('tab.information', {
+                url: '/information',
+                views: {
+                    'tab-information': {
+                        templateUrl: 'templates/information.html',
+                        controller: 'InformationCtrl'
+                    }
+                }
+            })
+
+            //资讯   竞彩足球
+            .state('tab.information.BiddingFootball', {
+                url: '/BiddingFootball',
+                cache: 'false',
+                templateUrl: 'templates/BiddingFootball.html',
+                controller: 'BiddingFootballCtrl'
+            })
+
+            //资讯   大乐透
+            .state('tab.information.SuperLotto', {
+                url: '/SuperLotto',
+                cache: 'false',
+                templateUrl: 'templates/SuperLotto.html',
+                controller: 'SuperLottoCtrl'
+            })
+
+            //竞彩足球 大乐透资讯详情
+            .state('tab.information.articleDetails', {
+                url: '/articleDetails',
+                cache: 'false',
+                templateUrl: 'templates/articleDetails.html'
+            })
+
+            //我的
+            .state('tab.mine', {
+                url: '/mine',
+                views: {
+                    'tab-mine': {
+                        templateUrl: 'templates/mine.html',
+                        controller: 'MineCtrl'
+                    }
+                }
+            });
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/sign-in');
+    });
+
+
+
+
+
+
+
+
+
+
+
---
+++ www/js/controllers.js
@@ -0,0 +1,95 @@
+angular.module('starter.controllers', [])
+
+    .controller('SignInCtrl', function ($scope, $state, $ionicPopup, $ionicLoading) {
+
+        $scope.signIn = function (user) {
+
+           /* if (typeof(user) == 'undefined') {
+
+                var alertPopup = $ionicPopup.alert({
+                    template: '<p style="text-align: center;">请填写登录信息</p>',
+                    okText: "确定"
+                });
+                return;
+            }
+            $ionicLoading.show();*/
+
+            $state.go('tab.home');
+
+            $ionicLoading.hide();
+
+        };
+    })
+
+    //首页
+    .controller('HomeCtrl', function ($scope) {
+
+    })
+
+    //***首页  大乐透下单详情
+    .controller('BigLottoCtrl', function ($scope) {
+    
+    })
+
+    //推荐
+    .controller('RecommendCtrl', function ($scope,$ionicPopover) {
+        $scope.popover = $ionicPopover.fromTemplateUrl('my-popover.html', {
+            scope: $scope
+        });
+        // .fromTemplate() 方法
+        var template = '<ion-popover-view style="width: 100px; height: 140px;">' +
+                ' <ion-content>' +
+            '        <h4>订单金额</h4>' +
+            '        <h4>订单金额</h4>' +
+            '        <h4>订单金额</h4>' +
+            '        <h4>订单金额</h4>' +
+                '</ion-content>' +
+            '</ion-popover-view>';
+
+        $scope.popover = $ionicPopover.fromTemplate(template, {
+            scope: $scope
+        });
+
+        $scope.openPopover = function($event) {
+            $scope.popover.show($event);
+        };
+        $scope.closePopover = function() {
+            $scope.popover.hide();
+        };
+        // 清除浮动框
+        $scope.$on('$destroy', function() {
+            $scope.popover.remove();
+        });
+        // 在隐藏浮动框后执行
+        $scope.$on('popover.hidden', function() {
+            // 执行代码
+        });
+        // 移除浮动框后执行
+        $scope.$on('popover.removed', function() {
+            // 执行代码
+        });
+
+
+
+    })
+
+    //资讯
+    .controller('InformationCtrl', function ($scope) {
+
+    })
+
+    //资讯   竞彩足球
+    .controller('BiddingFootballCtrl', function ($scope) {
+
+    })
+
+   //资讯   大乐透
+    .controller('SuperLottoCtrl', function ($scope) {
+
+    })
+
+
+    //我的
+    .controller('MineCtrl', function ($scope) {
+            /*djfgolsidafjgpon*/
+    });
