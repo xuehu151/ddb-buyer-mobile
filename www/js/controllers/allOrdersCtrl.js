69580531d8c809c1d85ce44c88d0bf72d982627d
liqiang <837769474@qq.com>
2017-07-26 20:44:50 +0800

    全部订单页面

--- www/js/controllers/allOrdersCtrl.js
+++ www/js/controllers/allOrdersCtrl.js
@@ -2,7 +2,15 @@
  * Created by admin on 2017/7/26.
  */
 angular.module ('starter.allOrdersCtrl', [])
-    //全部订单
+//全部订单
     .controller ('allOrdersCtrl', function ($scope) {
-    
+        $scope.tabNames = ['待付款','待出票', '待开奖', '待派奖', '已取票'];
+        $scope.selectIndex = 0;
+        $scope.activeTab = function (index) {
+            $scope.selectIndex = index;
+        };
+        
+        
+        
+        
     });
