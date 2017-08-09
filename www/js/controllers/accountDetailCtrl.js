41ecee4dcf6279542a26e49e70619ab42de63019
liqiang <173246lq>
2017-08-09 10:27:06 +0800

    提交resources文件

---
+++ www/js/controllers/accountDetailCtrl.js
@@ -0,0 +1,8 @@
+/**
+ * Created by admin on 2017/8/3.
+ */
+angular.module ('starter.accountDetailCtrl', [])
+//我的
+    .controller ('accountDetailCtrl', function ($scope, $state, $rootScope) {
+    
+    });
